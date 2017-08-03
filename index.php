7e4dafb8b34036fa8371935aa1fd5ab3c1ffaa57
RezaMehranfard <30705516+RezaMehranfard@users.noreply.github.com>
2017-08-04 02:33:45 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', 'XXX:XXX');
+define('BOT_TOKEN', '423560688:AAFrIZzFaHuo2EF2_6qYNyN2PJfobaWrwus');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@ function processMessage($message) {
     // process incoming message
     $boolean = file_get_contents('booleans.txt');
     $booleans= explode("\n",$boolean);
-    $admin = "آیدی عددی شما";
+    $admin = "250308439";
     $message_id = $message['message_id'];
     $rpto = $message['reply_to_message']['forward_from']['id'];
     $chat_id = $message['chat']['id'];
